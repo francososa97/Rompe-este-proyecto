817e3faa734b1527870ef551e41f0cd2bf794a8d
FrancoDamianSosa <146132072+FrancoDamianSosa@users.noreply.github.com>
2023-10-30 19:49:06 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.getElementById("cambiar-color");
+    const body = document.body;
+
+    button.addEventListener("click", function () {
+        const randomColor = getRandomColor();
+        body.style.backgroundColor = randomColor;
+    });
+
+    function getRandomColor() {
+        const letters = "0123456789ABCDEF";
+        let color = "#";
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+});
+console.log("test");
